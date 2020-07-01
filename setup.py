de6622ce2729fce42df2516f3d9ac34a79c24c6c
davina01130117 <64771256+davina01130117@users.noreply.github.com>
2020-07-02 02:14:10 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 from distutils.core import setup
 from Cython.Build import cythonize
 
-setup(name='bspt', ext_modules=cythonize("bspt.pyx"))
+setup(name='bspt', ext_modules=cythonize("bspt_slow.pyx"))
